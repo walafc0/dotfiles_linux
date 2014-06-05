2702b4de8ec8c2fc199bc06d626f9999d11fb394
walafc0 <devel@walafc0.org>
2014-06-05 14:02:01 +0200

    change in network.sh

--- .dmenu/network.sh
+++ .dmenu/network.sh
@@ -2,16 +2,19 @@
 
 # if VPN is on
 if ifconfig | grep tun0 > /dev/null; then
-  # IP=$(ifconfig | grep -A1 tun0 | tail -n1 | awk '{print $2}')
-  IP=$(ip r | grep $(ip r | grep default | awk '{print $3}') | grep -v src | tail -n1 | awk '{print $1}')
+  IP=$(ip r | grep kernel  | tail -n1 | awk '{print $9}')
   VPN="[vpn on]"
-
 else
-  IP=$(ip r | tail -n1 | awk '{print $9}')
+  IP=$(ip r | grep kernel  | awk '{print $9}')
   VPN="[vpn off]"
 fi
 
-DEV="$(ip r | grep kernel | tail -n1 | awk '{print $3}')"
-NET="$IP on $DEV $VPN"
+if [ ! -z $IP ]; then
+  DEV="$(ip r | grep kernel | tail -n1 | awk '{print $3}')"
+  NET="$IP on $DEV $VPN"
+else
+  NET="Disconnected"
+fi
+
 echo $NET
 
